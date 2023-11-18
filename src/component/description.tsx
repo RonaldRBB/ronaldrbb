63d5d0b5911137e25de598fd8b0b5d5d28b5c69f
Ronald Bello <ronaldbello2@gmail.com>
2023-11-17 23:28:26 -0300

    cosas

---
+++ src/component/description.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+class Description extends React.Component {
+    render() {
+        return (
+            <div className="ml-4">
+                <p>
+                    Desarrollador Full Stack apasionado con experiencia desde 2012
+                    en PHP, Python, Laravel, y Node.js. Especializado en el
+                    desarrollo de API REST, Microservicios, integración de
+                    servicios externos y aplicaciones web. Comprometido a
+                    contribuir positivamente a la calidad del producto y la
+                    experiencia del usuario.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Description;
