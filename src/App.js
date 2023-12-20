263f71ac92368d84be6989722d2d28a7fdf6b0de
Ronald Bello <ronaldbello2@gmail.com>
2023-12-20 14:13:28 -0300

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
+import Main from "./components/main";
+
 function App() {
-  return (
-    <div className="App">
-      <section class="section has-background-link-light">
-        <div class="container ">
-          <div class="columns is-centered">
-            <div class="column box has-background-white is-8 p-6">
-              {/* start */}
-              <div className="columns is-multiline is-mobile">
-                <div className="column is-12">
-                  <p>header</p>
+    return (
+        <div className="App">
+            <section class="section has-background-link-light">
+                <div class="container ">
+                    <div class="columns is-centered">
+                        <div class="column box has-background-white is-8 p-6">
+                            <Main />
+                        </div>
+                    </div>
                 </div>
-                <div className="column is-9">
-                  <p>content</p>
-                </div>
-                <div className="column is-3">
-                  <p>aside</p>
-                </div>
-              </div>
-              {/* end */}
-            </div>
-          </div>
-        </div>
-      </section>
-    </div >
-  );
+            </section>
+        </div >
+    );
 }
 
 export default App;
---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+export default function Main() {
+    return (
+        <div className="columns is-multiline is-mobile">
+            <div className="column is-12">
+                <p>header</p>
+            </div>
+            <div className="column is-9">
+                <p>content</p>
+            </div>
+            <div className="column is-3">
+                <p>aside</p>
+            </div>
+        </div>
+    );
+}
+
